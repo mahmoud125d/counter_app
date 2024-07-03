5ec25dd85cf8407511af01ca35f7cce16472affa
mhmoud <mhmoudsalah1997@gmail.com>
2024-07-03 05:55:38 +0300

    the app without mvvm

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -3,27 +3,80 @@ package com.example.counterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CounterAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text(text = "hello world")
+
+            TheCounterApp()
+
+        }
+    }
+
+    @Composable
+     fun TheCounterApp() {
+        val count  = remember {
+            mutableIntStateOf(0)
+        }
+        fun increment(){
+            count.intValue++
+        }
+        fun decrement(){
+            count.intValue--
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Count ${count.intValue}",
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Row {
+                Button(onClick = {
+                    increment()
+                }) {
+                    Text(text = "Increase")
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(onClick = {
+                    if(count.intValue>0){
+                        decrement()
+                    }
+                }) {
+                    Text(text = "Decrease")
                 }
             }
         }
+
     }
 }
 
